d7e671e367fd7dc77cb3605cdff9ec10fc695165
bharath-1999 <72253482+bharath-1999@users.noreply.github.com>
2022-06-13 13:46:37 +0530

    stock_buy_sell-1

---
+++ stockbuy_sell_1.js
@@ -0,0 +1,16 @@
+//space:O(1),time-complexity:O(n);
+//Leetcode link:https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var minSoFar=prices[0];
+    var ans=0;
+    for(let i=0;i<prices.length;i++){
+        ans=Math.max(ans,prices[i]-minSoFar);
+        minSoFar=Math.min(minSoFar,prices[i]);
+    }
+    return(ans);
+};
