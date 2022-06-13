e3d7e1664c44856df665a4851ac7df12ef473c25
bharath-1999 <72253482+bharath-1999@users.noreply.github.com>
2022-06-13 12:49:39 +0530

    Create Kadanes-algo.js

---
+++ Kadanes-algo.js
@@ -0,0 +1,24 @@
+//space:O(1),time: O(n)
+//leetcode link:https://leetcode.com/problems/maximum-subarray/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+   
+    var ans=nums[0];
+    var sumSofar=Math.max(ans,0);
+
+    for(let i=1;i<nums.length;i++){
+        
+        sumSofar+=nums[i];
+        
+        ans=Math.max(sumSofar,ans);
+        if(sumSofar<0)
+            sumSofar=0;
+    }
+    return(ans);
+    
+};
